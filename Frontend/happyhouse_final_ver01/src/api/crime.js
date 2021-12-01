fd64998e526e2d49da979a91482a565be240dfc9
unain-dev <daeun84366@gmail.com>
2021-12-01 23:30:11 +0900

    delete other version except final version

--- Frontend/happyhouse_final_ver01/src/api/crime.js
+++
@@ -1,9 +0,0 @@
-import { apiInstance } from "./index.js";
-
-const api = apiInstance();
-
-function getCrimeLevel(params, success, fail) {
-  api.get(`/map/crime`, { params: params }).then(success).catch(fail);
-}
-
-export default { getCrimeLevel };
